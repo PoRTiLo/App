f955b1a000bb200f82458283952b6cf4172bc1d6
portilo@seznam.cz <portilo@seznam.cz>
2015-01-23 00:02:24 +0100

    CHG Clean code

--- app/src/main/java/com/portilo/app/StatisticFragment.java
+++ app/src/main/java/com/portilo/app/StatisticFragment.java
@@ -13,22 +13,20 @@ public class StatisticFragment extends Fragment {
    * Returns a new instance of this fragment for the given section number.
    */
   public static StatisticFragment newInstance() {
-    StatisticFragment fragment = new StatisticFragment();
-    return fragment;
+    return new StatisticFragment();
   }
 
   public StatisticFragment() {}
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-    return rootView;
+    return  inflater.inflate(R.layout.fragment_statistic, container, false);
   }
 
   @Override
   public void onAttach(Activity activity) {
     super.onAttach(activity);
-    ((MainActivity) activity).onSectionAttached(0);
+    ((MainActivity) activity).onSectionAttached(1);
   }
 
 }
